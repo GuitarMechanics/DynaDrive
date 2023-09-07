089e016b7a44f2b0adca96597f87d7c497a278a8
GuitarMechanics <95960633+GuitarMechanics@users.noreply.github.com>
2023-09-07 15:14:38 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 솔루션용 nuget 설치 필요: metroui
+// https://luckygg.tistory.com/302
+
+namespace DynaDrive
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
