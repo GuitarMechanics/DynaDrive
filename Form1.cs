ecf83fd308449c537efaa639ccd9577c700d059d
kdj001124 <kdoungjun@khu.ac.kr>
2026-06-02 23:50:42 +0900

    hello kdj

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,8 @@ using System.Runtime.InteropServices;
 // 솔루션용 nuget 설치 필요: metroui
 // https://luckygg.tistory.com/302
 
+// hello kdj
+
 namespace DynaDrive
 {
     public partial class Form1 : MetroFramework.Forms.MetroForm
